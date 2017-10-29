a28007cf8dff8be296e3e0e9918ba91506525d75
gbourant <gbourant@gmail.com>
2017-10-29 09:08:29 +0200

    updade upload script

--- upload.sh
+++ upload.sh
@@ -3,10 +3,11 @@
 CONFIG_DIR=./configuration
 HOME=/home/gbourant
 
-cp -rp $HOME/.config/gtk-3.0 .
-cp -rp $HOME/.config/i3 .
-
 rm -rf $CONFIG_DIR
+
+cp -rp $HOME/.config/gtk-3.0 $CONFIG_DIR
+cp -rp $HOME/.config/i3 $CONFIG_DIR
+
 git add -A
-git commit
+git commit -m "updated from script"
 git push
