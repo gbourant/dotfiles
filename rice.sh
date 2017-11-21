6e7119c81895ab9794760712a79af1d3ce68e954
gbourant <gbourant@gmail.com>
2017-11-21 13:39:36 +0200

    created loop to copy and rice standalone config files

--- rice.sh
+++ rice.sh
@@ -8,7 +8,14 @@ configFolder=(gtk-3.0 i3)
 for folderName in "${configFolder[@]}"
 do
     rm -rf $HOME/.config/$folderName
+    mkdir -p $HOME/.config/$folderName
     cp -r  $CONFIG_DIR/.config/$folderName $HOME/.config
 done
 
-cp $CONFIG_DIR/.gtkrc-2.0 $HOME
+configFiles=(.gtkrc-2.0 .zshrc)
+
+for fileName in "${configFiles[@]}"
+do
+    rm -f $HOME/$fileName
+    cp $CONFIG_DIR/$fileName $HOME
+done
--- upload.sh
+++ upload.sh
@@ -5,15 +5,20 @@ HOME=/home/gbourant
 
 rm -rf $CONFIG_DIR
 
-configFolder=(gtk-3.0 i3)
+configFolders=(gtk-3.0 i3)
 
-for folderName in "${configFolder[@]}"
+for folderName in "${configFolders[@]}"
 do
     mkdir -p $CONFIG_DIR/.config/$folderName
     cp -r $HOME/.config/$folderName $CONFIG_DIR/.config
 done
 
-cp $HOME/.gtkrc-2.0 $CONFIG_DIR
+configFiles=(.gtkrc-2.0 .zshrc)
+
+for fileName in "${configFiles[@]}"
+do
+    cp $HOME/$fileName $CONFIG_DIR
+done
 
 git add -A
 git commit -m "updated from script"
